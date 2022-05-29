9680123be315ef8129c1479b5690cd56c9672ba4
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-05-29 12:59:24 -0300

    fazendo tab funcionar na força do ódio

--- front/assets/js/function.js
+++ front/assets/js/function.js
@@ -1,13 +1,38 @@
 let checkmark = document.getElementsByClassName("checkmark")[0];
 let check = document.getElementById("a");
 
-checkmark.addEventListener("click", () => {
-    if(checkmark.classList.contains("clicado")){
-        checkmark.classList.remove("clicado");
-        check.checked = false;
-    }
-    else{
-        checkmark.classList.add("clicado");
-        check.checked = true;
-    }
-});
+try{
+    checkmark.addEventListener("click", () => {
+        if(checkmark.classList.contains("clicado")){
+            checkmark.classList.remove("clicado");
+            check.checked = false;
+        }
+        else{
+            checkmark.classList.add("clicado");
+            check.checked = true;
+        }
+    });
+} catch(e){
+
+}
+
+let tabs = document.getElementsByClassName("nav-link");
+let panels = document.getElementsByClassName("tab-pane");
+
+for(let i = 0; i < tabs.length; i++){
+    tabs[i].addEventListener("click", () => {
+        let tabActive = document.getElementsByClassName("nav-link active")[0];
+        let panelActive = document.getElementsByClassName("tab-pane active")[0];
+
+        let tabSelect = tabs[i];
+        let panelSelect = panels[i];
+
+        tabActive.classList.remove("active");
+        panelActive.classList.remove("show");
+        panelActive.classList.remove("active");
+
+        tabSelect.classList.add("active");
+        panelSelect.classList.add("show");
+        panelSelect.classList.add("active");
+    });
+}
